4e3ca86c0951269e0829f7ddf4fa79d6a23cc7f7
W. Ryan Parker <rparker@indeed.com>
2017-10-16 12:11:36 -0500

    Finishing tutorial.

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -4,6 +4,7 @@ class AppointmentForm extends React.Component{
     /*this.onChange = this.onChange.bind(this)*/
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.updateApptTime = this.updateApptTime.bind(this)
   }
   handleChange(e){
     var name = e.target.name;
@@ -17,7 +18,21 @@ class AppointmentForm extends React.Component{
     this.props.onFormSubmit();
   }
   
+  updateApptTime(e){
+    var name = 'appt_time';
+    var obj = {}
+    if(obj[name] = e.toDate()){
+      this.props.onUserInput(obj);
+    }
+  }
+  
+  
+  
   render(){
+    var inputProps={
+      name: 'appt_time'
+    }
+    
     return(
       <div>
         <h2>Make a new appointment</h2>
@@ -27,7 +42,14 @@ class AppointmentForm extends React.Component{
             onChange={this.handleChange} />
           <input name='appt_time' placeholder='Date and time'
             value={this.props.appt_time}
-            onChange={this.handleChange} />
+            onChange={this.handleChange}
+            />
+            
+            <Datetime inputProps={inputProps}
+            onChange={this.updateApptTime}
+            input={false}
+            open={true} />
+            
           <input type='submit' value='Make an appointment' />
         </form>
       </div>
